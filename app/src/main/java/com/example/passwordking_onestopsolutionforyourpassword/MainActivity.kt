cde022beb99f85fa6887a85fc4934043ff1bda3e
Gourav Jamdhade <jamdhadegourav3@gmail.com>
2024-10-12 20:56:48 +0530

    Fixed some UI

--- app/src/main/java/com/example/passwordking_onestopsolutionforyourpassword/MainActivity.kt
+++ app/src/main/java/com/example/passwordking_onestopsolutionforyourpassword/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.example.passwordking_onestopsolutionforyourpassword
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.bg)
 
     }
 }
